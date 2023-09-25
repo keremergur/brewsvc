77c9a86729ce1292b9f5590c6ad8b441d70a8df5
Kerem Ergür <ergur.kerem@gmail.com>
2023-09-25 18:41:59 +0300

    colored status

--- script.js
+++ script.js
@@ -16,25 +16,37 @@ async function brewServices() {
                 type: 'checkbox',
                 choices: svcs.map(svc => {
                     return {
-                        name: `${svc.name}: ${svc.status}`,
+                        name: `${svc.name}: ${printStatus(svc)}`,
                         value: svc
                     };
                 })
             }
         ])
         .then(ans => {
-            console.log(ans);
+            // Promise.allSettled(ans.map(svc => {
+            //     return function(svc) {
+            //         let cmd = 'brew services ';
+            //         switch(svc.status) {
+            //             case 'none': str+=`start ${svc.name}`; break;
+            //             case 'started': str+=`stop ${svc.name}`; break;
+            //             case 'stopped': str+=`stop ${svc.name}`; break;
+            //         }
+            //         exec(cmd);
+            //     }
+            // }))
         });
 }
 
-// function printStatus(svc) {
-//     const yellow = '\x1b[33m';
-//     const green = '\x1b[33m';
-//     const red = '\x1b[33m';
-//     const end = '\x1b[0m';
-//     switch(svc.status) {
-//         case 'none': return ''
-//     }
-// }
+function printStatus(svc) {
+    const YELLOW = '\x1b[33m';  const GREEN = '\x1b[32m';
+    const RED = '\x1b[31m'; const END = '\x1b[0m';
+    let str = '';
+    switch(svc.status) {
+        case 'none': str+=YELLOW; break;
+        case 'started': str+=GREEN; break;
+        case 'stopped': str+=RED; break;
+    }
+    return str + svc.status + END;
+}
 
 brewServices();
