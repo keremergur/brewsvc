85a05aa21edf98bc360697ed3773b3a91b530bfc
Kerem Ergür <ergur.kerem@gmail.com>
2023-09-25 15:25:14 +0300

    async exec

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 import { exec } from 'child_process';
+import util from 'util';
+import inquirer from 'inquirer';
 
-exec('brew services list --json', (_1,out,_2) => {
-    console.log(out);
-});
+const asexec = util.promisify(exec);
+
+const { stdout } = await asexec('brew services list --json');
+const svcs = await JSON.parse(stdout);
+
+// await exec('brew services list --json', (_1,out,_2) => {
+//     svcs = JSON.parse(out);
+// });
+
+inquirer
+    .prompt([
+        {
+            name: 'servicepicker',
+            message: 'Please select service to enable/disable',
+            type: 'list',
+            choices: svcs.map(svc => {
+                return `${svc.name}: ${svc.status}`;
+            })
+        }
+    ])
+    .then()
