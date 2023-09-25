aa5dbab5eb47bb5afe1b152ffcd7420f4321beda
Kerem Ergür <ergur.kerem@gmail.com>
2023-09-25 15:26:27 +0300

    switch to checkbox

--- script.js
+++ script.js
@@ -7,16 +7,12 @@ const asexec = util.promisify(exec);
 const { stdout } = await asexec('brew services list --json');
 const svcs = await JSON.parse(stdout);
 
-// await exec('brew services list --json', (_1,out,_2) => {
-//     svcs = JSON.parse(out);
-// });
-
 inquirer
     .prompt([
         {
             name: 'servicepicker',
             message: 'Please select service to enable/disable',
-            type: 'list',
+            type: 'checkbox',
             choices: svcs.map(svc => {
                 return `${svc.name}: ${svc.status}`;
             })
