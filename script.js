a2942ca671214b8223c2f9e385e005cc9d0ac5c9
Kerem Ergür <ergur.kerem@gmail.com>
2023-09-25 18:19:57 +0300

    checkbox name value seperation

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ import inquirer from 'inquirer';
 
 const asexec = util.promisify(exec);
 
-const { stdout } = await asexec('brew services list --json');
-const svcs = await JSON.parse(stdout);
+async function brewServices() {
+    const { stdout } = await asexec('brew services list --json');
+    const svcs = await JSON.parse(stdout);
 
-inquirer
-    .prompt([
-        {
-            name: 'servicepicker',
-            message: 'Please select service to enable/disable',
-            type: 'checkbox',
-            choices: svcs.map(svc => {
-                return `${svc.name}: ${svc.status}`;
-            })
-        }
-    ])
-    .then()
+    inquirer
+        .prompt([
+            {
+                name: 'servicepicker',
+                message: 'Please select service to enable/disable',
+                type: 'checkbox',
+                choices: svcs.map(svc => {
+                    return {
+                        name: `${svc.name}: ${svc.status}`,
+                        value: svc
+                    };
+                })
+            }
+        ])
+        .then(ans => {
+            console.log(ans);
+        });
+}
+
+// function printStatus(svc) {
+//     const yellow = '\x1b[33m';
+//     const green = '\x1b[33m';
+//     const red = '\x1b[33m';
+//     const end = '\x1b[0m';
+//     switch(svc.status) {
+//         case 'none': return ''
+//     }
+// }
+
+brewServices();
